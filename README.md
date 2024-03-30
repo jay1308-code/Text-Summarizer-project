d7f16cebd147d383ae023cc7db28a21dab0876c5
Jay Pandya <jpandya065@gmail.com>
2024-03-30 19:19:29 +0530

    data validation completed

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 1. Update config.yaml
 2. Update params.yaml
 3. Update entity
-4. Update the configuration manager in src config.yaml
+4. Update the configuration manager in src config/configuration.py
 5. Update the components 
-6. Update the pipeline manager in src config.yaml
+6. Update the pipeline manager in src/pipeline
 7. Update the main.py
 8. Update the app.py
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingpipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
 
 try:
-    logger.info(f">>>>>>>>> stage {STAGE_NAME} Started <<<<<<<<<")
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Started <<<<<<<<<")
     data_ingestion_pipeline_obj = DataIngestionTrainingPipeline()
     data_ingestion_pipeline_obj.main()
-    logger.info(f">>>>>>>>> stage {STAGE_NAME} Completed <<<<<<")
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Completed <<<<<<")
+    logger.info("#"*60 + "\n")
+   
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Started <<<<<<<<<")
+    data_validation_pipeline_obj = DataValidationTrainingpipeline()
+    data_validation_pipeline_obj.main()
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Completed <<<<<<")
+    logger.info("#"*60 + "\n")
 
 except Exception as e:
     logger.exception(e)
