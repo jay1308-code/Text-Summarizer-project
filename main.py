00b6f2540d302077f5dc1aab72213ed2bba275c0
Jay Pandya <jpandya065@gmail.com>
2024-04-02 10:05:40 +0530

    01

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingpipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -24,6 +26,35 @@ try:
     logger.info(f">>>>>>>>>  {STAGE_NAME} Started <<<<<<<<<")
     data_validation_pipeline_obj = DataValidationTrainingpipeline()
     data_validation_pipeline_obj.main()
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Completed <<<<<<")
+    logger.info("#"*60 + "\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Started <<<<<<<<<")
+    data_transformation_pipeline_obj = DataTransformationTrainingPipeline()
+    data_transformation_pipeline_obj.main()
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Completed <<<<<<")
+    logger.info("#"*60 + "\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Training stage"
+
+try:
+    logger.info(f">>>>>>>>>  {STAGE_NAME} Started <<<<<<<<<")
+    model_training_pipeline_obj = ModelTrainingPipeline()
+    model_training_pipeline_obj.main()
     logger.info(f">>>>>>>>>  {STAGE_NAME} Completed <<<<<<")
     logger.info("#"*60 + "\n")
 
